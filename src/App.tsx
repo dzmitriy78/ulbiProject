714e3740a627bc0b80b7309de26db1631b4dee5a
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2023-04-01 20:16:08 +0300

    add: css Modules

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Counter} from "./component/Counter";
+import "./index.scss"
+
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            dsffsd
+            <Counter/>
+        </div>
+    )
+}
+
+export default App;
