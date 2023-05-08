ad563e021ea913878ff5349ee77e4ab1afe9c08d
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2023-05-08 13:18:01 +0300

    add: del LoginModal.module.scss

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./LoginModal.module.scss"
 import {Modal} from "shared/ui/Modal/Modal";
 import {LoginForm} from "../LoginForm/LoginForm";
 
@@ -11,7 +10,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({className, isOpen, onClose}: LoginModalProps) => {
     return (
-        <Modal className={classNames(cls.LoginModal, {}, [className])}
+        <Modal className={classNames("", {}, [className])}
                isOpen={isOpen}
                onClose={onClose}
                lazy
